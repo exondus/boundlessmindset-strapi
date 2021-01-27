91390397bad5c7fdedf6f7e3c6ac76f844f06962
exondus <lichaba@gmail.com>
2021-01-27 18:16:37 +0200

    Created content type

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'fantstq-dev.94g44.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'boundlessmind'),
-        username: env('DATABASE_USERNAME', 'lichaba'),
-        password: env('DATABASE_PASSWORD', '@WALKING91kgotsong'),
+        host: env("DATABASE_HOST", "fantstq-dev.94g44.mongodb.net"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "boundlessmind"),
+        username: env("DATABASE_USERNAME", "lichaba"),
+        password: env("DATABASE_PASSWORD", "@WALKING91kgotsong"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0a6dbf91-d513-40b6-9ab8-5a7e71059ab7'
+};
