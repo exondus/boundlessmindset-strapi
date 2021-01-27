260659ab7df5ec0d0aa0252b500a9b4c5f52ef88
exondus <lichaba@gmail.com>
2021-01-27 16:49:25 +0200

    Initial Commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'fantstq-dev.94g44.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'boundlessmind'),
+        username: env('DATABASE_USERNAME', 'lichaba'),
+        password: env('DATABASE_PASSWORD', '@WALKING91kgotsong'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a69dd3f0441740db60a615241ec1ab6'),
+    },
+  },
+});
